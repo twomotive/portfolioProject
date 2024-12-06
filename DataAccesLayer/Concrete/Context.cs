a9314d93744f23dc264034f8e69c06a279c3979e
twomotive <112391727+twomotive@users.noreply.github.com>
2024-12-06 10:40:27 +0300

    fix sql server name.

---
+++ DataAccesLayer/Concrete/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccesLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=REELESCAPE\\SQLEXPRESS;CoreProjeDB;integrated security=true");
+        }
+    }
+}
