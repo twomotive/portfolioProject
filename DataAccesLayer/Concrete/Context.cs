48030024d51cde0168287a0a490881ca0f49495f
twomotive <112391727+twomotive@users.noreply.github.com>
2024-12-06 12:25:14 +0300

    completed migration and reference operations: configured DbContext, set up inter-layer references, defined DbSets, and executed migrations

--- DataAccesLayer/Concrete/Context.cs
+++ DataAccesLayer/Concrete/Context.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EntityLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccesLayer.Concrete
@@ -11,7 +12,19 @@ namespace DataAccesLayer.Concrete
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=REELESCAPE\\SQLEXPRESS;CoreProjeDB;integrated security=true");
+            optionsBuilder.UseSqlServer("server=REELESCAPE\\SQLEXPRESS;database=CoreProjeDB;integrated security=true");
         }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
     }
 }
